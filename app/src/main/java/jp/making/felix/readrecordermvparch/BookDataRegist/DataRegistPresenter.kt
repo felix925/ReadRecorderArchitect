c90c4e9f2165c6992fe63f29c2a4b611554c6f3e
felix925 <0wg32248278026y@ezweb.ne.jp>
2020-01-31 22:56:11 +0900

    remake / DataResistPresenter

--- app/src/main/java/jp/making/felix/readrecordermvparch/BookDataRegist/DataRegistPresenter.kt
+++ app/src/main/java/jp/making/felix/readrecordermvparch/BookDataRegist/DataRegistPresenter.kt
@@ -1,14 +1,16 @@
 package jp.making.felix.readrecordermvparch.BookDataRegist
 
 import jp.making.felix.readrecordermvparch.data.Model.BaseRepository
+import jp.making.felix.readrecordermvparch.data.Model.ModelContract
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 private data class ErrorMessage(val message:String)
 
-class DataRegistPresenter(val BookRepository: BaseRepository,
+class DataRegistPresenter(val BookRepository: ModelContract.Repository,
                           val mView: DataRegistContract.View): DataRegistContract.Presenter{
     private val ValidateError = ErrorMessage("正しい情報を入力してください")
-    private val NotFoundError = ErrorMessage("本が見つかりませんでした")
-    private val CantRegistError = ErrorMessage("本が登録できませんでした")
+    private val NotFoundError = ErrorMessage("本がすでに登録されているか、見つかりませんでした")
 
     init{
         mView.presenter = this
@@ -20,26 +22,24 @@ class DataRegistPresenter(val BookRepository: BaseRepository,
     }
 
     override fun registData(isbn: String):Boolean{
-//        if (isbn.isEmpty()){
-//            mView.showEditError("ISBNを入力してください")
-//        }
-//        if(validationCheck(isbn)) {
-//            GlobalScope.launch {
-//                launch {
-//                    DataBase.registData(isbn, 1)
-//                }
-//            }
-//            return true
-//        }
-//        else{
-//            mView.showToast(ValidateError.message)
+        if (isbn.isEmpty()){
+            mView.showEditError("ISBNを入力してください")
+        }
+        if(validationCheck(isbn)) {
+            val flag = BookRepository.registData(isbn,0)
+            if(!flag){
+                mView.showToast(NotFoundError.message)
+            }
+            return flag
+        }
+        else{
+            mView.showToast(ValidateError.message)
             return false
-//        }
+         }
     }
     //入力されたISBN番号が正しいのバリデーションチェック
     private fun validationCheck(isbn: String):Boolean {
         val regex = Regex(pattern = "[0-9]+")
         return regex.matches(isbn)
-//        DataBase.registData(name,imageURL)
     }
 }
