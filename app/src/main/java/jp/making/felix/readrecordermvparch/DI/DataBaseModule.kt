cbbddc4fa39f4f147650fb1ca787d1bf11a3a4fa
felix925 <0wg32248278026y@ezweb.ne.jp>
2020-02-03 11:25:53 +0900

    fix / DataBaseModule

--- app/src/main/java/jp/making/felix/readrecordermvparch/DI/DataBaseModule.kt
+++ app/src/main/java/jp/making/felix/readrecordermvparch/DI/DataBaseModule.kt
@@ -6,18 +6,18 @@ import jp.making.felix.readrecordermvparch.data.Model.BookRepository
 import jp.making.felix.readrecordermvparch.data.Model.LocalBookModel
 import jp.making.felix.readrecordermvparch.data.Model.RemoteBookModel
 
-@Module
-class DataBaseModule {
-    @Provides
-    fun provideRepository(): BookRepository{
-        return BookRepository(provideLocalRepository(),provideRemoteRepository())
-    }
-    @Provides
-    fun provideLocalRepository():LocalBookModel{
-        return LocalBookModel()
-    }
-    @Provides
-    fun provideRemoteRepository():RemoteBookModel{
-        return RemoteBookModel()
-    }
-}
+//@Module
+//class DataBaseModule {
+//    @Provides
+//    fun provideRepository(): BookRepository{
+//        return BookRepository(provideLocalRepository(),provideRemoteRepository())
+//    }
+//    @Provides
+//    fun provideLocalRepository():LocalBookModel{
+//        return LocalBookModel()
+//    }
+//    @Provides
+//    fun provideRemoteRepository():RemoteBookModel{
+//        return RemoteBookModel()
+//    }
+//}
