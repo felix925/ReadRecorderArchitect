489c8f8d179d8a6295829247ac14fac8e7d77499
felix925 <0wg32248278026y@ezweb.ne.jp>
2020-02-03 11:25:02 +0900

    fix / RemoteBookModel

--- app/src/main/java/jp/making/felix/readrecordermvparch/data/Model/RemoteBookModel.kt
+++ app/src/main/java/jp/making/felix/readrecordermvparch/data/Model/RemoteBookModel.kt
@@ -1,57 +1,58 @@
 package jp.making.felix.readrecordermvparch.data.Model
 
+import android.annotation.SuppressLint
+import android.util.Log
 import com.squareup.moshi.Moshi
 import io.realm.RealmList
 import jp.making.felix.readrecordermvparch.data.Book
 import jp.making.felix.readrecordermvparch.data.GoogleBook.GoogleBook
 import jp.making.felix.readrecordermvparch.data.Page
-import kotlinx.coroutines.handleCoroutineException
-import okhttp3.Call
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.Response
+import jp.making.felix.readrecordermvparch.data.UpdateDate
+import kotlinx.coroutines.*
+import okhttp3.*
+import okhttp3.internal.wait
 import java.io.IOException
-import java.lang.Exception
 import java.text.SimpleDateFormat
 import java.util.*
-import javax.inject.Singleton
 
 class RemoteBookModel:ModelContract.RemoteData{
     override suspend fun searchData(isbn: String, type: Int): Book {
-        lateinit var book: Book
+        lateinit var book:Book
         when(type){
             0 -> book = searchFromGoogle(isbn)
             else -> throw Error("BOOK IS UNDEFINED")
         }
         return book
     }
-    fun searchFromGoogle(isbn: String):Book{
+    private suspend fun searchFromGoogle(isbn: String):Book {
         val client = OkHttpClient()
         val request = Request.Builder()
-            .url("https://www.googleapis.com/books/v1/volumes?q=isbn:{${isbn}}")
+            .url("https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}")
             .build()
         val moshiAdapter = Moshi.Builder().build().adapter(GoogleBook::class.java)
-        lateinit var book:Book
-        client.newCall(request).enqueue(object : okhttp3.Callback{
-            @Throws(IOException::class)
-            override fun onResponse(call: Call,response: Response){
-                val resString = response.body()?.string()!!
-                val bookData = moshiAdapter.fromJson(resString) ?: throw Error("Book is not Found")
-                bookData.items[0].apply {
-                    val bookName = this.volumeInfo.title
-                    val imageURL = this.volumeInfo.imageLinks.toString()
-                    val maxPage = this.volumeInfo.pageCount.toString()
-                    val lastLog = getDate()
-                    book = Book(UUID.randomUUID().toString(),bookName,imageURL,lastLog,
-                        RealmList(Page(0)),maxPage,false)
-                }
-            }
-            override fun onFailure(call: Call, e: IOException) {
-                throw Error("Request is failed")
-            }
-        })
-        return book
+        var book: Book = Book("ERROR")
+        return withContext(Dispatchers.IO) {
+            val response = client.newCall(request).execute()
+            val resString = response.body!!.string()
+            val bookData =
+                moshiAdapter.fromJson(resString) ?: throw Error("Book is not Found")
+            val bookName = bookData.items[0].volumeInfo.title
+            val imageURL = bookData.items[0].volumeInfo.imageLinks.thumbnail
+            val maxPage = bookData.items[0].volumeInfo.pageCount.toString()
+            val updateDate = UpdateDate(getDate())
+            Book(
+                UUID.randomUUID().toString(),
+                bookName,
+                imageURL,
+                RealmList(updateDate),
+                RealmList(Page(0)),
+                maxPage,
+                false
+            )
+        }
     }
+
+    @SuppressLint("SimpleDateFormat")
     private fun getDate():String{
         return SimpleDateFormat("yyyy-MM-dd").toString()
     }
