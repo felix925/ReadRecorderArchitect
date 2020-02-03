3c1ecbe07127efb3d26d04559c53db93413d3a82
felix925 <0wg32248278026y@ezweb.ne.jp>
2020-02-03 11:19:17 +0900

    fix / BaseRepository

--- app/src/main/java/jp/making/felix/readrecordermvparch/data/Model/BaseRepository.kt
+++ app/src/main/java/jp/making/felix/readrecordermvparch/data/Model/BaseRepository.kt
@@ -3,8 +3,8 @@ package jp.making.felix.readrecordermvparch.data.Model
 import jp.making.felix.readrecordermvparch.data.Book
 
 interface BaseRepository {
-    suspend fun getAllData():List<Book>
-    suspend fun deleteData(id:String)
-    suspend fun searchData(id:String): Book
-    suspend fun updateData(id:Int,value:String)
+    fun getAllData():List<Book>
+    fun deleteData(id:String)
+    fun searchData(id:String): Book
+    fun updateData(id:Int,pageValue:String,thought:String)
 }
