57211eb172ca7feba25591e70fafada08315875b
felix925 <0wg32248278026y@ezweb.ne.jp>
2020-02-07 17:22:46 +0900

    add / thoughtListAdapter

---
+++ app/src/main/java/jp/making/felix/readrecordermvparch/BookDataView/thoughtListAdapter.kt
@@ -0,0 +1,13 @@
+package jp.making.felix.readrecordermvparch.BookDataView
+
+import android.widget.ListView
+import jp.making.felix.readrecordermvparch.data.Logs
+import jp.making.felix.readrecordermvparch.data.Page
+
+class thoughtListAdapter (list:ListView){
+    fun setDataToList(thought:Array<Logs>,page:Array<Page>){
+        for(i in page){
+
+        }
+    }
+}
