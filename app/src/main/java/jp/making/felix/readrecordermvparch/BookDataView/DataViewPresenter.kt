4350962c7c47e8d67886403b23c7b47ef807a916
felix925 <0wg32248278026y@ezweb.ne.jp>
2020-02-07 17:21:56 +0900

    fix / DataViewPresenter

--- app/src/main/java/jp/making/felix/readrecordermvparch/BookDataView/DataViewPresenter.kt
+++ app/src/main/java/jp/making/felix/readrecordermvparch/BookDataView/DataViewPresenter.kt
@@ -1,19 +1,29 @@
 package jp.making.felix.readrecordermvparch.BookDataView
 
 import jp.making.felix.readrecordermvparch.data.Book
+import jp.making.felix.readrecordermvparch.data.Logs
 import jp.making.felix.readrecordermvparch.data.Model.BaseRepository
+import jp.making.felix.readrecordermvparch.data.Page
 
 class DataViewPresenter(val BookRepository: BaseRepository,
                         val mView: DataViewContract.View):DataViewContract.Presenter{
     init{
         mView.presenter = this
     }
+
     override fun start(){
         mView.showProgress()
         mView.deleteProgress()
     }
-    //TODO getDataの実装
-    fun getData(count: String): Book {
-        return Book()
+    override fun getPageData(id: String): Pair<Array<Page>, Int> {
+        val books = BookRepository.searchData(id)
+        val log = books.pages.toTypedArray()
+        val maxPage = books.maxPage.toInt()
+        return Pair(log,maxPage)
+    }
+
+    override fun getThoughtData(id: String): Array<Logs> {
+        val books = BookRepository.searchData(id)
+        return books.readLog.toTypedArray()
     }
 }
