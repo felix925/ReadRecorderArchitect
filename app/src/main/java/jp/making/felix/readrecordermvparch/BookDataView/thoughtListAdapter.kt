ffe22128310a12c02346d046668e1b8b3d15f364
felix925 <0wg32248278026y@ezweb.ne.jp>
2020-02-18 15:49:20 +0900

    add / thoughtListAdapter

--- app/src/main/java/jp/making/felix/readrecordermvparch/BookDataView/thoughtListAdapter.kt
+++ app/src/main/java/jp/making/felix/readrecordermvparch/BookDataView/thoughtListAdapter.kt
@@ -1,13 +1,44 @@
 package jp.making.felix.readrecordermvparch.BookDataView
 
-import android.widget.ListView
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.TextView
+import jp.making.felix.readrecordermvparch.R
 import jp.making.felix.readrecordermvparch.data.Logs
 import jp.making.felix.readrecordermvparch.data.Page
 
-class thoughtListAdapter (list:ListView){
-    fun setDataToList(thought:Array<Logs>,page:Array<Page>){
-        for(i in page){
+class thoughtListAdapter(context: Context, val logs: List<Logs>, val page: List<Page>): BaseAdapter() {
+    private val inflater = LayoutInflater.from(context)
 
-        }
+
+    override fun getItem(position: Int) = logs[position].logData
+
+    override fun getItemId(position: Int) = position.toLong()
+
+    override fun getCount() = logs.size
+
+
+    private fun createView(parent: ViewGroup?) : View {
+        val view = inflater.inflate(R.layout.thought_item, parent, false)
+        view.tag = ViewHolder(view)
+        return view
+    }
+
+    private class ViewHolder(view: View) {
+        val thoughtPage = view.findViewById<TextView>(R.id.thoughtPage)
+        val thoughtText = view.findViewById<TextView>(R.id.thoughtText)
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val view = convertView ?: createView(parent)
+
+        val viewHolder = view.tag as ViewHolder
+
+        viewHolder.thoughtPage.text = page[position].pageData.toString()
+        viewHolder.thoughtText.text = getItem(position)
+        return view
     }
 }
