4115de4cd61ae3f30dad236e31304642690c2b94
felix925 <45941545+felix925@users.noreply.github.com>
2020-03-08 15:50:34 +0900

    fix / DataRegistPresenter

--- app/src/main/java/jp/making/felix/readrecordermvparch/BookDataRegist/DataRegistPresenter.kt
+++ app/src/main/java/jp/making/felix/readrecordermvparch/BookDataRegist/DataRegistPresenter.kt
@@ -1,43 +1,55 @@
 package jp.making.felix.readrecordermvparch.BookDataRegist
 
 import jp.making.felix.readrecordermvparch.data.Model.BaseRepository
-import jp.making.felix.readrecordermvparch.data.Model.ModelContract
-import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
 
 private data class ErrorMessage(val message:String)
 
-class DataRegistPresenter(val BookRepository: ModelContract.Repository,
-                          val mView: DataRegistContract.View): DataRegistContract.Presenter{
+class DataRegistPresenter(val BookRepository: BaseRepository): DataRegistContract.Presenter{
+    //TODO ここのnullをどうにかしたい(nullsafetyが活かせてない)
+    private var mView:DataRegistContract.View? = null
+    override val coroutineContext: CoroutineContext = Job() + Dispatchers.Main
     private val ValidateError = ErrorMessage("正しい情報を入力してください")
     private val NotFoundError = ErrorMessage("本がすでに登録されているか、見つかりませんでした")
 
-    init{
-        mView.presenter = this
+    override fun takeView(view: DataRegistContract.View) {
+        mView = view
     }
 
     override fun start() {
-        mView.showProgress()
-        mView.deleteProgress()
+        mView?.showProgress()
+        mView?.deleteProgress()
     }
 
     override fun registData(isbn: String):Boolean{
         if (isbn.isEmpty()){
-            mView.showEditError("ISBNを入力してください")
+            mView?.showEditError("ISBNを入力してください")
         }
         if(validationCheck(isbn)) {
-            val flag = BookRepository.registData(isbn,0)
-            
-            if(!flag){
-                mView.showToast(NotFoundError.message)
-                return false
+            var flag:Boolean = false
+            launch {
+                mView?.showProgress()
+                runCatching { BookRepository.registData(isbn, 0) }
+                    .onSuccess { flag = true }
+                    .onFailure {
+                        mView?.showToast(NotFoundError.message)
+                        flag = false
+                    }
+                mView?.deleteProgress()
             }
             return flag
         }
         else{
-            mView.showToast(ValidateError.message)
+            mView?.showToast(ValidateError.message)
             return false
-         }
+        }
+    }
+
+    override fun loadBooks() {
+
     }
     //入力されたISBN番号が正しいのバリデーションチェック
     private fun validationCheck(isbn: String):Boolean {
