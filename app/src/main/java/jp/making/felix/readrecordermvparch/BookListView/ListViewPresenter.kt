342bd5e05d99f6389fb6bafd3bdf80dfd0be642a
felix925 <45941545+felix925@users.noreply.github.com>
2020-03-12 01:58:38 +0900

    include / Dagger

--- app/src/main/java/jp/making/felix/readrecordermvparch/BookListView/ListViewPresenter.kt
+++ app/src/main/java/jp/making/felix/readrecordermvparch/BookListView/ListViewPresenter.kt
@@ -2,19 +2,30 @@ package jp.making.felix.readrecordermvparch.BookListView
 
 import jp.making.felix.readrecordermvparch.data.BookModel.Book
 import jp.making.felix.readrecordermvparch.data.Model.BaseRepository
+import jp.making.felix.readrecordermvparch.data.Model.BookRepository
 import kotlinx.coroutines.*
+import javax.inject.Inject
 import kotlin.coroutines.CoroutineContext
 
-class ListViewPresenter(val BookRepository:BaseRepository,
-                        val mView: ListViewContract.View):ListViewContract.Presenter{
+class ListViewPresenter @Inject constructor(
+    val BookRepository:BookRepository
+):ListViewContract.Presenter{
+
+    private var mView: ListViewContract.View? = null
     override val coroutineContext: CoroutineContext = Job() + Dispatchers.Main
-    init{
-        mView.presenter = this
+
+//    init{
+//        mView?.presenter = this
+//    }
+
+    override fun attachView(view:ListViewContract.View){
+        mView = view
     }
+
     override fun start(){
-        mView.showProgress()
-        mView.showAllBooks(getAllData())
-        mView.deleteProgress()
+        mView?.showProgress()
+        mView?.showAllBooks(getAllData())
+        mView?.deleteProgress()
     }
 
     fun getAllData():List<Book>{
